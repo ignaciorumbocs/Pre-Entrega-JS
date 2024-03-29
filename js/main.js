2749289b64f509e2d4fcfe32924517e3381cd1a9
Ignacio Rumbo <nachorumbo01@gmail.com>
2024-03-29 03:46:39 -0300

    finalizacion de segunda Pre-Entrega

--- js/main.js
+++ js/main.js
@@ -1,28 +1,83 @@
-function promedioEdades() {
-    let suma = 0;
-    for (let cantidadEdades = 1; cantidadEdades <= 5; cantidadEdades++) {
-        let edadIngresada = parseInt(prompt("Ingrese Edad"));
-
-        if (bandera = isNaN(edadIngresada)) {
-            alert("La edad ingresada no es valida")
-            cantidadEdades--
-        
-        }
-        if (edadIngresada > 0 && edadIngresada <= 100) {
-
-
-            suma += edadIngresada
-
-
-        } 
-    }
-    return suma / 5;
+const arrayProductos = [
+  { nombre: "Teclado Dragonborn", precio: 5000 },
+  { nombre: "Final Mouse", precio: 4000 },
+  { nombre: "Auriculares Razer", precio: 6000 },
+  { nombre: "Sky Pad", precio: 2000 },
+  { nombre: "Motinor Zowie", precio: 7000 },
+];
+const arrayCarrito = [];
+///////////////////////// FUNCIONES ////////////////////////////
+function mostrarCarrito() {
+  let carrito = "Productos del Carrito:\n";
+  arrayCarrito.forEach(function (producto) {
+    carrito += producto.nombre + " - $" + producto.precio + "\n";
+  });
+  return carrito;
 }
 
-let promedio = promedioEdades()
+function agregarProducto(iProducto) {
+  arrayCarrito.push(arrayProductos[iProducto]);
+}
 
-alert("El promedio de las edades es " + promedio)
+function quitarUltimoProducto() {
+  arrayCarrito.pop();
+}
 
+function limpiarArray() {
+  arrayCarrito = [];
+}
 
+function quitarProducto(i) {
+  arrayCarrito.splice(i, 1);
+}
 
+function mostrarProductos() {
+  let lista = "Lista de productos:\n";
+  arrayProductos.forEach(function (elemento, i) {
+    lista += i + 1 + ". " + elemento.nombre + " - $" + elemento.precio + "\n";
+  });
 
+  return lista;
+}
+
+function realizarCompra() {
+  let total = 0;
+  while (true) {
+    let productoSeleccionado = prompt(
+      mostrarProductos() +
+        "\n Ingrese el numero del producto que desea. 'fin' para finalizar compra o 'eliminar' para quitar un producto"
+    );
+    if (productoSeleccionado.toLowerCase() === "fin") {
+      break;
+    } else if (productoSeleccionado.toLowerCase() === "eliminar") {
+      let productoEliminado = prompt(
+        "Ingrese el numero del producto que desee eliminar"
+      );
+      let iEliminar = parseInt(productoEliminado) - 1;
+      if (iEliminar >= 0 && iEliminar < arrayCarrito.length) {
+        total -= arrayCarrito[iEliminar].precio;
+        quitarProducto(iEliminar);
+        alert("El producto ha sido eliminado");
+      } else {
+        alert("Opción Incorrecta");
+      }
+    } else {
+      let iAgregar = parseInt(productoSeleccionado) - 1;
+      if (iAgregar >= 0 && iAgregar < arrayProductos.length) {
+        total += arrayProductos[iAgregar].precio;
+        agregarProducto(iAgregar);
+        alert(
+          "Has agregado un " + arrayProductos[iAgregar].nombre + " al carrito"
+        );
+      } else {
+        alert("Opción Incorrecta");
+      }
+    }
+  }
+  alert(
+    mostrarCarrito() + "---------------------------\n" + "El total es $" + total
+  );
+  mostrarProductos();
+}
+
+realizarCompra();
